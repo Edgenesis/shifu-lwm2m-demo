7b1e5f1844d8165b5446034c535b2ee75bc32d15
rhoninlee <rhoninl@hotmail.com>
2024-07-30 18:20:16 +0800

    update customized_handlers

--- mqtt-lwm2m/customized_handlers.py
+++ mqtt-lwm2m/customized_handlers.py
@@ -1,5 +1,17 @@
 import json
 
+# input
+# {"xAngle":-73.08371735,"yAngle":-90}
+
+
+def XAngle(raw_data):
+    return json.loads(raw_data["mqtt_message"])["xAngle"]
+
+
+def YAngle(raw_data):
+    return json.loads(raw_data["mqtt_message"])["yAngle"]
+
 
 def raw_value(raw_data):
+    print(raw_data["mqtt_message"])
     return raw_data["mqtt_message"]
--- mqtt-lwm2m/main.py
+++
@@ -1,3 +0,0 @@
-from customized_handlers import raw_value
-
-print(raw_value({"mqtt_message": "Hello, World!"}), end='')
