166031b8a9e3808daf057e5ee91f07838af6c38b
rhoninlee <rhoninl@hotmail.com>
2024-07-30 18:01:26 +0800

    update customized deviceshifu for mqtt to handle data

---
+++ mqtt-lwm2m/customized_handlers.py
@@ -0,0 +1,5 @@
+import json
+
+
+def raw_value(raw_data):
+    return raw_data["mqtt_message"]
---
+++ mqtt-lwm2m/main.py
@@ -0,0 +1,3 @@
+from customized_handlers import raw_value
+
+print(raw_value({"mqtt_message": "Hello, World!"}), end='')
